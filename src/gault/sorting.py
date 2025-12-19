4fe27b516f0fff70a5d596b025bdcb661dd99468
Xavier Barbosa <clint.northwood@gmail.com>
2025-12-19 14:57:03 +0100

    refactor: sort imports

--- src/gault/sorting.py
+++ src/gault/sorting.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, Literal, assert_never, TypeAlias
+from typing import TYPE_CHECKING, Any, Literal, TypeAlias, assert_never
 
 from .types import AsRef
 
