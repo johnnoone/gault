6fc154c325ab013d0f3d0081879015a1347ab6c6
Xavier Barbosa <clint.northwood@gmail.com>
2025-12-27 08:21:23 +0100

    feat: new geo objects

--- src/gault/geo.py
+++ src/gault/geo.py
@@ -1,30 +1,230 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Any, TypeAlias
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Any, Literal, NamedTuple, TypeAlias
 
 if TYPE_CHECKING:
-    from .types import Context
+    from .types import Context, MongoExpression
 
     Geo: TypeAlias = "Box" | "Center" | "CenterSphere" | "GeoJSON"
-    Box: TypeAlias = Any
-    Center: TypeAlias = Any
-    CenterSphere: TypeAlias = Any
-    GeoJSON: TypeAlias = Any
 
 
-def compile_geo(value: Geo, *, context: Context) -> Any:
+def compile_geo(value: Geo | MongoExpression, *, context: Context) -> Any:
     match value:
-        case {"$box": _}:
+        case {"$box": _} | {"$center": _} | {"$centerSphere": _}:
             # https://www.mongodb.com/docs/manual/reference/operator/query/box/
-            return value
-        case {"$center": _}:
             # https://www.mongodb.com/docs/manual/reference/operator/query/center/
-            return value
-        case {"$centerSphere": _}:
             # https://www.mongodb.com/docs/manual/reference/operator/query/centerSphere/
             return value
         case {"$geometry": _}:
             # https://www.mongodb.com/docs/manual/reference/operator/query/geometry/
             return value
+        case {
+            "type": "Point"
+            | "LineString"
+            | "Polygon"
+            | "MultiPoint"
+            | "MultiLineString"
+            | "MultiPolygon"
+            | "GeometryCollection"
+        }:
+            return {"$geometry": value}
+        case GeoJSON():
+            return value.compile_expression(context=context)
+        case Box() | Center() | CenterSphere():
+            return value.compile_expression(context=context)
+        case _:
+            raise NotImplementedError
+
+
+class GeoJSON(ABC):
+    @abstractmethod
+    def compile_expression(self, *, context: Context) -> MongoExpression: ...
+
+    @abstractmethod
+    def get_coordinates(self) -> Any: ...
+
+
+@dataclass
+class Point(GeoJSON):
+    x: float
+    y: float
+
+    def compile_expression(self, *, context: Context) -> MongoExpression:
+        return {
+            "$geometry": {
+                "type": "Point",
+                "coordinates": self.get_coordinates(),
+            }
+        }
+
+    def get_coordinates(self) -> Any:
+        return compile_position(self)
+
+
+@dataclass
+class LineString(GeoJSON):
+    points: list[Point]
+
+    def compile_expression(self, *, context: Context) -> MongoExpression:
+        return {
+            "$geometry": {
+                "type": "LineString",
+                "coordinates": self.get_coordinates(),
+            }
+        }
+
+    def get_coordinates(self) -> Any:
+        return [point.get_coordinates() for point in self.points]
+
+
+@dataclass
+class Polygon(GeoJSON):
+    line_strings: list[LineString]
+    crs: Literal["urn:x-mongodb:crs:strictwinding:EPSG:4326"] | None = None
+
+    def compile_expression(self, *, context: Context) -> MongoExpression:
+        if name := self.crs:
+            custom = {
+                "crs": {
+                    "type": "name",
+                    "properties": {"name": name},
+                },
+            }
+        else:
+            custom = {}
+        return {
+            "$geometry": {
+                "type": "Polygon",
+                "coordinates": self.get_coordinates(),
+            }
+            | custom
+        }
+
+    def get_coordinates(self) -> Any:
+        return [line_string.get_coordinates() for line_string in self.line_strings]
+
+
+@dataclass
+class MultiPoint(GeoJSON):
+    points: list[Point]
+
+    def compile_expression(self, *, context: Context) -> MongoExpression:
+        return {
+            "$geometry": {
+                "type": "MultiPoint",
+                "coordinates": self.get_coordinates(),
+            }
+        }
+
+    def get_coordinates(self) -> Any:
+        return [point.get_coordinates() for point in self.points]
+
+
+@dataclass
+class MultiLineString(GeoJSON):
+    line_strings: list[LineString]
+
+    def compile_expression(self, *, context: Context) -> MongoExpression:
+        return {
+            "$geometry": {
+                "type": "MultiLineString",
+                "coordinates": self.get_coordinates(),
+            }
+        }
+
+    def get_coordinates(self) -> Any:
+        return [line_string.get_coordinates() for line_string in self.line_strings]
+
+
+@dataclass
+class MultiPolygon(GeoJSON):
+    polygons: list[Polygon]
+
+    def compile_expression(self, *, context: Context) -> MongoExpression:
+        return {
+            "$geometry": {
+                "type": "MultiPolygon",
+                "coordinates": self.get_coordinates(),
+            }
+        }
+
+    def get_coordinates(self) -> Any:
+        return [polygon.get_coordinates() for polygon in self.polygons]
+
+
+@dataclass
+class GeometryCollection(GeoJSON):
+    geometries: list[GeoJSON]
+
+    def compile_expression(self, *, context: Context) -> MongoExpression:
+        return {
+            "$geometry": {
+                "type": "GeometryCollection",
+                "geometries": [
+                    geometry.compile_expression(context=context)["$geometry"]  # type: ignore[call-overload, index]
+                    for geometry in self.geometries
+                ],
+            }
+        }
+
+    def get_coordinates(self) -> Any:
+        return [geometry.get_coordinates() for geometry in self.geometries]
+
+
+@dataclass
+class Box:
+    bottom_left_coordinates: Point
+    upper_right_coordinates: Point
+
+    def compile_expression(self, *, context: Context) -> MongoExpression:
+        return {
+            "$box": [
+                compile_position(self.bottom_left_coordinates),
+                compile_position(self.upper_right_coordinates),
+            ],
+        }
+
+
+@dataclass
+class Center:
+    coordinates: Point
+    radius: float
+
+    def compile_expression(self, *, context: Context) -> MongoExpression:
+        return {
+            "$center": [
+                compile_position(self.coordinates),
+                self.radius,
+            ],
+        }
+
+
+@dataclass
+class CenterSphere:
+    coordinates: Point
+    radius: float
+
+    def compile_expression(self, *, context: Context) -> MongoExpression:
+        return {
+            "$centerSphere": [
+                compile_position(self.coordinates),
+                self.radius,
+            ],
+        }
+
+
+class Coordinates(NamedTuple):
+    x: float
+    y: float
+
+
+def compile_position(input: Coordinates | Point, /) -> list[float]:
+    match input:
+        case Coordinates(x, y):
+            return [x, y]
+        case Point(x, y):
+            return [x, y]
         case _:
             raise NotImplementedError
---
+++ tests/geo_test.py
@@ -0,0 +1,476 @@
+from gault.geo import compile_geo
+from gault.geo import (
+    Point,
+    LineString,
+    Polygon,
+    MultiLineString,
+    MultiPoint,
+    MultiPolygon,
+    GeometryCollection,
+    Box,
+    Center,
+    CenterSphere,
+)
+
+
+def test_compile_point(context):
+    geo = Point(40, 5)
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "Point",
+            "coordinates": [40, 5],
+        }
+    }
+
+
+def test_compile_line_string(context):
+    geo = LineString([Point(40, 5), Point(41, 6)])
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "LineString",
+            "coordinates": [[40, 5], [41, 6]],
+        }
+    }
+
+
+def test_compile_polygon_single_ring(context):
+    geo = Polygon([LineString([Point(0, 0), Point(3, 6), Point(6, 1), Point(0, 0)])])
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "Polygon",
+            "coordinates": [[[0, 0], [3, 6], [6, 1], [0, 0]]],
+        }
+    }
+
+
+def test_compile_polygon_multi_rings(context):
+    geo = Polygon(
+        [
+            LineString([Point(0, 0), Point(3, 6), Point(6, 1), Point(0, 0)]),
+            LineString([Point(2, 2), Point(3, 3), Point(4, 2), Point(2, 2)]),
+        ],
+    )
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [[0, 0], [3, 6], [6, 1], [0, 0]],
+                [[2, 2], [3, 3], [4, 2], [2, 2]],
+            ],
+        }
+    }
+
+
+def test_compile_multi_point(context):
+    geo = MultiPoint(
+        [
+            Point(-73.9580, 40.8003),
+            Point(-73.9498, 40.7968),
+            Point(-73.9737, 40.7648),
+            Point(-73.9814, 40.7681),
+        ],
+    )
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "MultiPoint",
+            "coordinates": [
+                [-73.9580, 40.8003],
+                [-73.9498, 40.7968],
+                [-73.9737, 40.7648],
+                [-73.9814, 40.7681],
+            ],
+        }
+    }
+
+
+def test_compile_multi_line_string(context):
+    geo = MultiLineString(
+        [
+            LineString([Point(-73.96943, 40.78519), Point(-73.96082, 40.78095)]),
+            LineString([Point(-73.96415, 40.79229), Point(-73.95544, 40.78854)]),
+            LineString([Point(-73.97162, 40.78205), Point(-73.96374, 40.77715)]),
+            LineString([Point(-73.97880, 40.77247), Point(-73.97036, 40.76811)]),
+        ],
+    )
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "MultiLineString",
+            "coordinates": [
+                [[-73.96943, 40.78519], [-73.96082, 40.78095]],
+                [[-73.96415, 40.79229], [-73.95544, 40.78854]],
+                [[-73.97162, 40.78205], [-73.96374, 40.77715]],
+                [[-73.97880, 40.77247], [-73.97036, 40.76811]],
+            ],
+        }
+    }
+
+
+def test_compile_multi_polygon(context):
+    geo = MultiPolygon(
+        [
+            Polygon(
+                [
+                    LineString(
+                        [
+                            Point(-73.958, 40.8003),
+                            Point(-73.9498, 40.7968),
+                            Point(-73.9737, 40.7648),
+                            Point(-73.9814, 40.7681),
+                            Point(-73.958, 40.8003),
+                        ]
+                    ),
+                ]
+            ),
+            Polygon(
+                [
+                    LineString(
+                        [
+                            Point(-73.958, 40.8003),
+                            Point(-73.9498, 40.7968),
+                            Point(-73.9737, 40.7648),
+                            Point(-73.958, 40.8003),
+                        ]
+                    )
+                ]
+            ),
+        ]
+    )
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "MultiPolygon",
+            "coordinates": [
+                [
+                    [
+                        [-73.958, 40.8003],
+                        [-73.9498, 40.7968],
+                        [-73.9737, 40.7648],
+                        [-73.9814, 40.7681],
+                        [-73.958, 40.8003],
+                    ]
+                ],
+                [
+                    [
+                        [-73.958, 40.8003],
+                        [-73.9498, 40.7968],
+                        [-73.9737, 40.7648],
+                        [-73.958, 40.8003],
+                    ]
+                ],
+            ],
+        }
+    }
+
+
+def test_compile_geometry_collection(context):
+    geo = GeometryCollection(
+        [
+            MultiPoint(
+                [
+                    Point(-73.9580, 40.8003),
+                    Point(-73.9498, 40.7968),
+                    Point(-73.9737, 40.7648),
+                    Point(-73.9814, 40.7681),
+                ],
+            ),
+            MultiLineString(
+                [
+                    LineString(
+                        [
+                            Point(-73.96943, 40.78519),
+                            Point(-73.96082, 40.78095),
+                        ]
+                    ),
+                    LineString(
+                        [
+                            Point(-73.96415, 40.79229),
+                            Point(-73.95544, 40.78854),
+                        ]
+                    ),
+                    LineString(
+                        [
+                            Point(-73.97162, 40.78205),
+                            Point(-73.96374, 40.77715),
+                        ]
+                    ),
+                    LineString(
+                        [
+                            Point(-73.97880, 40.77247),
+                            Point(-73.97036, 40.76811),
+                        ]
+                    ),
+                ],
+            ),
+        ]
+    )
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "GeometryCollection",
+            "geometries": [
+                {
+                    "type": "MultiPoint",
+                    "coordinates": [
+                        [-73.9580, 40.8003],
+                        [-73.9498, 40.7968],
+                        [-73.9737, 40.7648],
+                        [-73.9814, 40.7681],
+                    ],
+                },
+                {
+                    "type": "MultiLineString",
+                    "coordinates": [
+                        [[-73.96943, 40.78519], [-73.96082, 40.78095]],
+                        [[-73.96415, 40.79229], [-73.95544, 40.78854]],
+                        [[-73.97162, 40.78205], [-73.96374, 40.77715]],
+                        [[-73.97880, 40.77247], [-73.97036, 40.76811]],
+                    ],
+                },
+            ],
+        }
+    }
+
+
+####
+
+
+def test_compile_geojson_point(context):
+    geo = {"type": "Point", "coordinates": [40, 5]}
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "Point",
+            "coordinates": [40, 5],
+        }
+    }
+
+
+def test_compile_geojson_line_string(context):
+    geo = {"type": "LineString", "coordinates": [[40, 5], [41, 6]]}
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "LineString",
+            "coordinates": [[40, 5], [41, 6]],
+        }
+    }
+
+
+def test_compile_geojson_polygon_single_ring(context):
+    geo = {"type": "Polygon", "coordinates": [[[0, 0], [3, 6], [6, 1], [0, 0]]]}
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "Polygon",
+            "coordinates": [[[0, 0], [3, 6], [6, 1], [0, 0]]],
+        }
+    }
+
+
+def test_compile_geojson_polygon_multi_rings(context):
+    geo = {
+        "type": "Polygon",
+        "coordinates": [
+            [[0, 0], [3, 6], [6, 1], [0, 0]],
+            [[2, 2], [3, 3], [4, 2], [2, 2]],
+        ],
+    }
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [[0, 0], [3, 6], [6, 1], [0, 0]],
+                [[2, 2], [3, 3], [4, 2], [2, 2]],
+            ],
+        }
+    }
+
+
+def test_compile_geojson_multi_point(context):
+    geo = {
+        "type": "MultiPoint",
+        "coordinates": [
+            [-73.9580, 40.8003],
+            [-73.9498, 40.7968],
+            [-73.9737, 40.7648],
+            [-73.9814, 40.7681],
+        ],
+    }
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "MultiPoint",
+            "coordinates": [
+                [-73.9580, 40.8003],
+                [-73.9498, 40.7968],
+                [-73.9737, 40.7648],
+                [-73.9814, 40.7681],
+            ],
+        }
+    }
+
+
+def test_compile_geojson_multi_line_string(context):
+    geo = {
+        "type": "MultiLineString",
+        "coordinates": [
+            [[-73.96943, 40.78519], [-73.96082, 40.78095]],
+            [[-73.96415, 40.79229], [-73.95544, 40.78854]],
+            [[-73.97162, 40.78205], [-73.96374, 40.77715]],
+            [[-73.97880, 40.77247], [-73.97036, 40.76811]],
+        ],
+    }
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "MultiLineString",
+            "coordinates": [
+                [[-73.96943, 40.78519], [-73.96082, 40.78095]],
+                [[-73.96415, 40.79229], [-73.95544, 40.78854]],
+                [[-73.97162, 40.78205], [-73.96374, 40.77715]],
+                [[-73.97880, 40.77247], [-73.97036, 40.76811]],
+            ],
+        }
+    }
+
+
+def test_compile_geojson_multi_polygon(context):
+    geo = {
+        "type": "MultiPolygon",
+        "coordinates": [
+            [
+                [
+                    [-73.958, 40.8003],
+                    [-73.9498, 40.7968],
+                    [-73.9737, 40.7648],
+                    [-73.9814, 40.7681],
+                    [-73.958, 40.8003],
+                ]
+            ],
+            [
+                [
+                    [-73.958, 40.8003],
+                    [-73.9498, 40.7968],
+                    [-73.9737, 40.7648],
+                    [-73.958, 40.8003],
+                ]
+            ],
+        ],
+    }
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "MultiPolygon",
+            "coordinates": [
+                [
+                    [
+                        [-73.958, 40.8003],
+                        [-73.9498, 40.7968],
+                        [-73.9737, 40.7648],
+                        [-73.9814, 40.7681],
+                        [-73.958, 40.8003],
+                    ]
+                ],
+                [
+                    [
+                        [-73.958, 40.8003],
+                        [-73.9498, 40.7968],
+                        [-73.9737, 40.7648],
+                        [-73.958, 40.8003],
+                    ]
+                ],
+            ],
+        }
+    }
+
+
+def test_compile_geojson_geometry_collection(context):
+    geo = {
+        "type": "GeometryCollection",
+        "geometries": [
+            {
+                "type": "MultiPoint",
+                "coordinates": [
+                    [-73.9580, 40.8003],
+                    [-73.9498, 40.7968],
+                    [-73.9737, 40.7648],
+                    [-73.9814, 40.7681],
+                ],
+            },
+            {
+                "type": "MultiLineString",
+                "coordinates": [
+                    [[-73.96943, 40.78519], [-73.96082, 40.78095]],
+                    [[-73.96415, 40.79229], [-73.95544, 40.78854]],
+                    [[-73.97162, 40.78205], [-73.96374, 40.77715]],
+                    [[-73.97880, 40.77247], [-73.97036, 40.76811]],
+                ],
+            },
+        ],
+    }
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$geometry": {
+            "type": "GeometryCollection",
+            "geometries": [
+                {
+                    "type": "MultiPoint",
+                    "coordinates": [
+                        [-73.9580, 40.8003],
+                        [-73.9498, 40.7968],
+                        [-73.9737, 40.7648],
+                        [-73.9814, 40.7681],
+                    ],
+                },
+                {
+                    "type": "MultiLineString",
+                    "coordinates": [
+                        [[-73.96943, 40.78519], [-73.96082, 40.78095]],
+                        [[-73.96415, 40.79229], [-73.95544, 40.78854]],
+                        [[-73.97162, 40.78205], [-73.96374, 40.77715]],
+                        [[-73.97880, 40.77247], [-73.97036, 40.76811]],
+                    ],
+                },
+            ],
+        }
+    }
+
+
+def test_box(context):
+    geo = Box(Point(1, 2), Point(3, 4))
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$box": [
+            [1, 2],
+            [3, 4],
+        ]
+    }
+
+
+def test_center(context):
+    geo = Center(Point(1, 2), radius=42)
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$center": [
+            [1, 2],
+            42,
+        ]
+    }
+
+
+def test_center_sphere(context):
+    geo = CenterSphere(Point(1, 2), radius=42)
+    expr = compile_geo(geo, context=context)
+    assert expr == {
+        "$centerSphere": [
+            [1, 2],
+            42,
+        ]
+    }
